74684f4d66966f1c28d1b7befe2ee9388dff0aed
Rayess-Massih <143374992+RAYESS-MASSIH@users.noreply.github.com>
2023-10-14 10:20:21 +0430

    Add files via upload

---
+++ open.py
@@ -0,0 +1,30 @@
+import requests
+
+cookies = {
+    'datr': 'cMQpZdwqAO-AbYsz13XL-S7w',
+}
+
+headers = {
+    'authority': 'm.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9,fa-AF;q=0.8,fa;q=0.7',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=cMQpZdwqAO-AbYsz13XL-S7w',
+    'dpr': '1.875',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"SM-A125F"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"12.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
+}
+
+response = requests.get('https://m.facebook.com/', cookies=cookies, headers=headers)
